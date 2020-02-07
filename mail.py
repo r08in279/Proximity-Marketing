ad30dff59ec7ff1dd855675e6252cf21032456b6
Robin Balyan <robingautamsisauli@gmail.com>
2020-02-07 15:41:54 +0530

    first commit

---
+++ mail.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 14:54:18 2019
+
+@author: robin
+"""
+
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('robingautamsisauli@gmail.com')
+EMAIL_PASSWORD = os.environ.get('robmail#71')
+
+contacts = ['robinsingh_sa@srmuniv.edu.in']
+
+msg = EmailMessage()
+msg['Subject'] = 'These Offers may excite you!'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = contacts
+
+msg.set_content('Hurry!! 70% off on Shirts !!')
+
+msg.add_alternative("""\
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style="color:SlateGray;">This is an HTML Email!</h1>
+    </body>
+</html>
+""", subtype='html')
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
